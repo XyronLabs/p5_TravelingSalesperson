26969f39cdd097c666eeea5c5a7919d4783084c9
ACharLuk <acharluk@gmail.com>
2017-02-11 13:57:22 +0100

    Initial cities

--- sketch.js
+++ sketch.js
@@ -1,9 +1,22 @@
+var cities = [];
+var numCities = 5;
+
 function setup() {
     createCanvas(800, 480);
 
+    for (var i = 0; i < numCities; i++) {
+        var x = random(0, width);
+        var y = random(0, height);
+        cities.push(new createVector(x, y));
+    }
 }
 
 function draw() {
     background(0);
     
+    stroke(255);
+    strokeWeight(8);
+    for (var i = 0; i < numCities; i++) {
+        point(cities[i].x, cities[i].y);
+    }
 }
